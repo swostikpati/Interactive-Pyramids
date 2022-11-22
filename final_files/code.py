ee2ead92abf0dc69d80d549dbc9a53bf9926c8c6
Swostik Pati <67205637+swostikpati@users.noreply.github.com>
2022-11-22 21:24:18 +0400

    Update code.py

--- final_files/code.py
+++ final_files/code.py
@@ -1,5 +1,53 @@
+# Write your code here :-)
+# SPDX-FileCopyrightText: 2018 Anne Barela for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+print("starting up")
+import neoring_colors
 import musicCode
-print("Hello! Music Playing...")
 
-for i in range(5):
-    musicCode.play(i)
+#neoring.lightLED()
+
+import time
+import board
+from analogio import AnalogIn
+
+light_sensor1 = AnalogIn(board.A1)  # potentiometer connected to A1, power & ground
+
+delta_threshold = 1000
+brightness = light_sensor1.value
+pbrightness = brightness
+
+# colorArr = [0, 10, 30, 85, 100, 137, 170, 213]
+
+#suspenseful
+# colorArr = [0, 10, 30, 50] 
+
+#happy
+colorArr = [100, 137, 170, 213] 
+
+
+
+
+#neoring_colors.lightOff()
+i = 0
+flag = True;
+while True:
+
+    brightness = light_sensor1.value
+    print((light_sensor1.value))      # Display value
+    if(brightness - pbrightness > delta_threshold and flag):
+        ##also add a max ceiling beyond which it will work
+        print(light_sensor1.value)
+        print("Changed")
+        ##function call
+        #music.musicOn()
+        musicCode.play(i, colorArr[i])
+        i=(i+1)%4
+        flag = False
+#         print(i)
+    else:
+        flag = True
+    pbrightness = brightness
+
+    time.sleep(1)                   # Wait a bit before checking all again
